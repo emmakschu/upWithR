94f80fdb0a78260aaf34bbacc03412d77b2c4e6d
Michael Schumacher <mschu528@gmail.com>
2017-01-24 13:13:29 -0600

    initialize repository

---
+++ 1_1.R
@@ -0,0 +1,24 @@
+# Up and Running with R
+# 24 Jan 2017
+#
+# 1_1.R
+# Basic Math
+
+2 + 2 # calculates the sum
+print(2 + 2) # prints the sum
+
+1:100 # prints the range
+
+print("Hello world!") # prints the string
+
+x <- 1:5 # assigns values 1-5 to x
+
+x # displays value of x
+
+y <- c(6, 7, 8, 9, 10) # concatenate y with each value
+
+x + y # displays sum of each element in vectors x and y
+
+x <- x * 2 # multiply each element in x by 2 and assign
+
+x + y # displays new sum of (2x) + y
